ffe73ce9cf2361e299a451c458390f9358974677
Shashwot Pradhan <shashwotpradhan@gmail.com>
2023-08-25 08:46:31 +0545

    find us

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { FooterOverlay, Newsletter } from '../../components';
+import {FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 import './Footer.css';
 
 const Footer = () => (
